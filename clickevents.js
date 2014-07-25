78444a8b9d6e4892650104ea244897fc8e999e3d
pedromcunha <pedro.spinsomewebs@gmail.com>
2014-07-25 17:03:12 -0400

    First Commit : JS event handling

---
+++ clickevents.js
@@ -0,0 +1,8 @@
+var myElement = document.getElementById('button2');
+var myElement2 = document.getelementById('button');
+myElement.onclick = function() {
+    alert('testing');
+};
+
+var sidebar = document.getElementById('sidebar');
+var myNewElement = document.createElement('li');
